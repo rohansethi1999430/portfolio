a65ee2a28a87d63395cd92871267f862938e169e
rohansethi <rohansethi1999430@gmail.com>
2024-07-18 21:56:08 -0400

    pushing the workexp files and made some changes in the about section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,52 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
-
+import { motion } from "framer-motion"
 
 
 type Props = {}
 
 function Header({}: Props) {
   return (
-    <header className=' sticky top-0 flex items-start  p-5 justify-between max-w-7xl mx-auto'>
-<div className='flex flex-row items-center'>
+    <header className=' sticky top-0 flex items-start p-5 justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+<motion.div 
+initial = {{
+    x: -500,
+    opacity:0,
+    scale:0.5
+}}
+animate = {{
+    x:0,
+    opacity:1,
+    scale:1
+}}
+transition={{
+    duration:1.5,
+}}
+className='flex flex-row items-center'>
           {/* social icons */}
-          {/* <SocialIcon url="http://twitter.com/jaketrent" 
-          fgColor='gray' 
-          bgColor=' transparent'/>
-          <SocialIcon url="http://facebook.com/jaketrent"
-           fgColor='gray' 
-           bgColor=' transparent' /> */}
           <SocialIcon url="https://www.linkedin.com/in/rohan-sethi-a27107178/" 
            fgColor='gray' 
            bgColor=' transparent'/>
           <SocialIcon url="https://github.com/rohansethi1999430"
            fgColor='gray' 
            bgColor=' transparent' />
-        </div>
-        <div className=' flex flex-row items-center text-gray-300 cursor-pointers'>
+        </motion.div>
+        <motion.div 
+        initial = {{
+            x:500,
+            opacity:0,
+            scale:0.5
+        }}
+        animate = {{
+            x :0,
+            scale:1,
+            opacity:1
+        }}
+        transition={{
+            duration:1.5
+        }}
+        
+        className=' flex flex-row items-center text-gray-300 cursor-pointers'>
             <SocialIcon
             className=' cursor-pointer'
             network='email'
@@ -33,7 +56,7 @@ function Header({}: Props) {
             <p className=' uppercase hidden md:inline-flex text-sm text-gray-400'>
             Get In Touch
            </p>
-           </div>
+           </motion.div>
 
     </header>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,37 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import Head from "next/head";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import WorkExperience from "@/components/WorkExperience";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-   <div>
+   <div className=" bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll
+    z-0">
+
+    <Head>
+      <title>Rohan's Portfolio</title>
+    </Head>
     <Header/>
+
+    <section id = 'hero' className=" snap-start">
+
+      <Hero/>
+
+    </section>
+
+    <section id = 'about' className=" snap-center">
+      <About/>
+    </section>
+
+    <section id = "workExperience">
+      <WorkExperience/>
+    </section>
+
    </div>
   );
 }
