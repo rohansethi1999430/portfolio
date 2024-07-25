08f5900b68259a59321f411ef42bc5974e287482
rohansethi <rohansethi1999430@gmail.com>
2024-07-24 23:46:14 -0400

    added image to experience card and also added framer motion

--- components/ExperienceCards.tsx
+++ components/ExperienceCards.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import { motion } from "framer-motion"
 type Props = {}
 
 function ExperienceCards({}: Props) {
   return (
-    <div>ExperienceCards</div>
+    <article>
+        <motion.img
+        initial={{
+            y: -100,
+            opacity :0,
+        }}
+        transition={{
+            duration:1.2
+        }}
+        whileInView={{
+            opacity:1,
+            y:0,
+        }}
+        viewport={{
+            once:true
+        }}
+        className=' h-60 w-60 rounded-full xl:w-[200px] xl:h-[200px]'
+        src="/Dell_Logo.png" alt="" />
+
+    </article>
   )
 }
 
