367577d1e458dfd5509f7d9f5c9bd4ae10786923
rohansethi <rohansethi1999430@gmail.com>
2024-07-28 21:24:48 -0400

    added sanity logo as test and also adjusted the size of the logo

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 
-type Props = {}
 
-function Skill({}: Props) {
+type Props = {
+    directionLeft?: boolean;
+}
+
+function Skill({directionLeft}: Props) {
   return (
-    <div>Skill</div>
+    <div className=' group relative flex cursor-pointer'>
+        <motion.img
+        initial = {{
+            x:directionLeft ? -200:200,
+            opacity:0
+        }}
+        transition={{
+            duration:1
+        }}
+        whileInView={{opacity:1,x:0}}
+        src='/sanity_logo.png'
+        className=' h-20 w-20'
+        />
+    </div>
   )
 }
 
