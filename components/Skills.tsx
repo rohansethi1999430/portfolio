d8a001d745d435b3cfc8a1377b96d09bfe2aff7c
rohansethi <rohansethi1999430@gmail.com>
2024-07-28 20:58:34 -0400

    made some changes in skill component

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { motion } from 'framer-motion';
 
 type Props = {}
@@ -6,12 +6,11 @@ type Props = {}
 function Skills({}: Props) {
   return (
     <motion.div className=' text-center flex relative flex-col md:text-left xl:flex-row max-w-[2000px] 
-    xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto  items-center'>
-        <h3 className='absolute top-10 uppercase tracking-[20px] text-gray-500 text-2xl mb-20'>Skills</h3>
-        <h3>Hover over a skill for proficiency</h3>
-        
+    xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
+        <h3 className=' absolute top-32 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over a skill for current proficiency</h3>
     </motion.div>
   )
 }
 
-export default Skills
+export default Skills;
