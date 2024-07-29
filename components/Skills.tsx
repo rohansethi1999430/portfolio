2f8c1a9f29435d7317f0f2bdc9bd23b91f072d7d
rohansethi <rohansethi1999430@gmail.com>
2024-07-28 21:05:01 -0400

    created a new Skill.tsx file

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import Skill from './Skill';
 
 type Props = {}
 
@@ -9,6 +10,29 @@ function Skills({}: Props) {
     xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
         <h3 className=' absolute top-32 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over a skill for current proficiency</h3>
+
+        <div className=' grid grid-cols-4 gap-5'>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+
+        </div>
+
+
+
     </motion.div>
   )
 }
