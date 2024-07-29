d4c690314fc2ee495493b0c5507b0b2eb37ab188
rohansethi <rohansethi1999430@gmail.com>
2024-07-28 21:09:28 -0400

    added motion div props into the Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -7,7 +7,13 @@ type Props = {}
 function Skills({}: Props) {
   return (
     <motion.div className=' text-center flex relative flex-col md:text-left xl:flex-row max-w-[2000px] 
-    xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
+    xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'
+    
+    initial ={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:1.5}}
+    
+    >
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
         <h3 className=' absolute top-32 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over a skill for current proficiency</h3>
 
