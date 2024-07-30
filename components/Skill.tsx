5130a3070b46b5cd2cf9115e02da6148cde25e94
rohansethi <rohansethi1999430@gmail.com>
2024-07-29 21:04:44 -0400

    made some UI changes in Skill.tsx

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -22,6 +22,13 @@ function Skill({directionLeft}: Props) {
         className=' h-24 w-24 rounded-full border border-gray-500 object-cover filter group-hover:grayscale
          transition duration-300 ease-in-out'
         />
+        
+        <div className=' absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out
+         group-hover:bg-white h-24 w-24 rounded-full border z-0'>
+            <div className=' flex items-center justify-center h-full'>
+                <p className=' text-3xl font-bold text-black opacity-100'>100%</p>
+            </div>
+        </div>
     </div>
   )
 }
