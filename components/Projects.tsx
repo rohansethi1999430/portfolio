7820d42940a04941a1575fe9be4b21feb24a2249
rohansethi <rohansethi1999430@gmail.com>
2024-08-11 20:55:50 -0400

    added styling to projects screen

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,7 +8,20 @@ function Projects({}: Props) {
     <div className=' h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full 
     mx-auto justify-evenly items-center z-0'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
-        <div className=' w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12'></div>
+
+
+        <div className=' relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
+            {/* projects */}
+            {/* projects */}
+            {/* projects */}
+            {/* projects */}
+            {/* projects */}
+            {/* projects */}
+        </div>
+
+        <div className=' w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 
+        h-[500px] -skew-y-12'>
+        </div>
     </div>
   )
 }
